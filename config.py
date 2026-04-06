d4bea034fc2d030c939539468921b5b01cb39488
hamoniii <hamonionsol@gmail.com>
2026-04-06 17:03:15 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+API_ID = 32246406
+API_HASH = "a343c73a5423e452442d49b37d93a355"
+
+SOURCE_GROUPS = [
+"https://t.me/solearlytrending",
+"https://t.me/solwhaletrending",
+"https://t.me/solhousesignal"
+]
+
+DESTINATION_GROUP = "https://t.me/+3ANqf9_yG1NmNjlk"
+
+DEXSCREENER_API = "https://api.dexscreener.com/latest/dex/tokens/"
+
+SIMILARITY_THRESHOLD = 0.7
